6748f3eca3d9d274d47578af215cbaf7aca39b89
Himanshi Sharma <himanshish456@gmail.com>
2024-08-03 16:56:13 +0530

    spring without boot

---
+++ DemoSpring/src/main/java/org/example/App.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml"); // ApplicationContext is an Interface, we can't instantiate it, but ClassPathXmlApplicationContext extends ApplicationContext interface, so we can instantiate through it.
+        Dev obj = context.getBean(Dev.class);
+        obj.build();
+    }
+}
