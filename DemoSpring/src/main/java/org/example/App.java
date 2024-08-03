4015683a6199a179493aa32e9e2cb121c61d3d69
Himanshi Sharma <himanshish456@gmail.com>
2024-08-03 17:10:44 +0530

    spring xml config

--- DemoSpring/src/main/java/org/example/App.java
+++ DemoSpring/src/main/java/org/example/App.java
@@ -12,7 +12,8 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml"); // ApplicationContext is an Interface, we can't instantiate it, but ClassPathXmlApplicationContext extends ApplicationContext interface, so we can instantiate through it.
-        Dev obj = context.getBean(Dev.class);
-        obj.build();
+//        Dev obj = context.getBean(Dev.class);
+//        Dev obj = (Dev) context.getBean("dev"); // name dev will return object that why write Dev before context.getBeans()
+//        obj.build();
     }
 }
--- DemoSpring/src/main/java/org/example/Dev.java
+++ DemoSpring/src/main/java/org/example/Dev.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Dev {
 
+    public Dev(){
+        System.out.println("Dev constructor");
+    }
+
     public void build(){
         System.out.println("working on project");
     }
