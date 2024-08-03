368bdef60704d03ccee7afd5f3c74f5c72a5ed7e
Himanshi Sharma <himanshish456@gmail.com>
2024-08-03 18:05:29 +0530

    autowire in spring

--- DemoSpring/src/main/java/org/example/Dev.java
+++ DemoSpring/src/main/java/org/example/Dev.java
@@ -2,7 +2,8 @@ package org.example;
 
 public class Dev {
 
-    private Laptop laptop;
+//    private Laptop laptop;
+    private Computer comp;
 
     private int age;
 
@@ -10,10 +11,10 @@ public class Dev {
         System.out.println("Dev constructor");
     }
 
-    public Dev(int age) {
-        this.age = age;
-        System.out.println("Dev parameterized constructor");
-    }
+//    public Dev(int age) {
+//        this.age = age;
+//        System.out.println("Dev parameterized constructor");
+//    }
 
     public int getAge() {
         return age;
@@ -23,21 +24,31 @@ public class Dev {
         this.age = age;
     }
 
-    public Laptop getLaptop() {
-        return laptop;
+//    public Laptop getLaptop() {
+//        return laptop;
+//    }
+//
+//    public void setLaptop(Laptop laptop) {
+//        this.laptop = laptop;
+//    }
+
+//    public Dev(Laptop laptop) {
+//        this.laptop = laptop;
+//        System.out.println("laptop parametrized constructor");
+//    }
+
+
+    public Computer getComp() {
+        return comp;
     }
 
-    public void setLaptop(Laptop laptop) {
-        this.laptop = laptop;
-    }
-
-    public Dev(Laptop laptop) {
-        this.laptop = laptop;
-        System.out.println("laptop parametrized constructor");
+    public void setComp(Computer comp) {
+        this.comp = comp;
     }
 
     public void build(){
         System.out.println("working on project");
-        laptop.compile();
+//        laptop.compile();
+        comp.compile();
     }
 }
