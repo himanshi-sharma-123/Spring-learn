c33ea7061dd3b74f59f6110ca9f1793888e9eb8e
Himanshi Sharma <himanshish456@gmail.com>
2024-08-04 11:29:27 +0530

    spring web http methods get and post

--- simpleWebApp/src/main/java/com/example/simpleWebApp/controller/ProductController.java
+++ simpleWebApp/src/main/java/com/example/simpleWebApp/controller/ProductController.java
@@ -3,8 +3,7 @@ package com.example.simpleWebApp.controller;
 import com.example.simpleWebApp.model.Product;
 import com.example.simpleWebApp.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,8 +13,19 @@ public class ProductController {
     @Autowired
     ProductService service;
 
-    @RequestMapping("/products")
+    @GetMapping("/products")
     public List<Product> getProducts(){
         return service.getProducts();
+    }
+
+    @GetMapping("/products/{prodId}")
+    public Product getProductById(@PathVariable int prodId){  // applied PathVariable because we are accepting the value from pathVariable
+        return service.getProductById(prodId);
+    }
+
+    @PostMapping("/products")
+    public void addProduct(@RequestBody Product prod){
+        System.out.println(prod);
+        service.addProduct(prod);
     }
 }
--- simpleWebApp/src/main/java/com/example/simpleWebApp/service/ProductService.java
+++ simpleWebApp/src/main/java/com/example/simpleWebApp/service/ProductService.java
@@ -3,14 +3,25 @@ package com.example.simpleWebApp.service;
 import com.example.simpleWebApp.model.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class ProductService {
 
-    List<Product> products = Arrays.asList(new Product(101, "Iphone", 50000), new Product(102, "Canon Camera", 70000), new Product(103, "Shure mic", 40000));
+    List<Product> products = new ArrayList<>(Arrays.asList(new Product(101, "Iphone", 50000), new Product(102, "Canon Camera", 70000), new Product(103, "Shure mic", 40000)));
     public List<Product> getProducts(){
        return products;
+    }
+
+    public Product getProductById(int prodId) {
+        return products.stream()
+                .filter(p -> p.getProdId() == prodId)
+                .findFirst().orElse(new Product(100, "No item", 0));
+    }
+
+    public void addProduct(Product prod){
+        products.add(prod);
     }
 }
