2847657205f2c3b58d1de13ac5853696590a2105
Himanshi Sharma <himanshish456@gmail.com>
2024-08-04 11:52:29 +0530

    delete data

--- simpleWebApp/src/main/java/com/example/simpleWebApp/controller/ProductController.java
+++ simpleWebApp/src/main/java/com/example/simpleWebApp/controller/ProductController.java
@@ -33,4 +33,9 @@ public class ProductController {
     public void updateProduct(@RequestBody Product prod){ // RequestBody is responsible for get data in the request and sent that in the reference
         service.updateProduct(prod);
     }
+
+    @DeleteMapping("/products/{prodId}")
+    public void deleteProduct(@PathVariable int prodId){
+        service.deleteProduct(prodId);
+    }
 }
