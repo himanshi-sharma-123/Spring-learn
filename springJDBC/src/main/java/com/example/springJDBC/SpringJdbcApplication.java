96643a3e848ee706866d3f08873cc145d65a64cb
Himanshi Sharma <himanshish456@gmail.com>
2024-08-05 22:01:11 +0530

    spring boot jdbc using jdbc template

---
+++ springJDBC/src/main/java/com/example/springJDBC/SpringJdbcApplication.java
@@ -0,0 +1,26 @@
+package com.example.springJDBC;
+
+import com.example.springJDBC.model.Alien;
+import com.example.springJDBC.repo.AlienRepo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringJdbcApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(SpringJdbcApplication.class, args);
+		Alien alien1 = context.getBean(Alien.class);
+		alien1.setId(111);
+		alien1.setName("Himanshi");
+		alien1.setTech("Java");
+
+		AlienRepo repo = context.getBean(AlienRepo.class);
+		repo.save(alien1);
+
+		System.out.println(repo.findAll());
+
+	}
+
+}
---
+++ springJDBC/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into alien (id, name, tech) values(101, 'Himanshi', 'Java');
+insert into alien (id, name, tech) values(102, 'Archna', 'Blockchain');
