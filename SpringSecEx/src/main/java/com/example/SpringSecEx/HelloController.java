cb205a394f80355cbad9b9086fff361daddb4d2d
Himanshi Sharma <himanshish456@gmail.com>
2024-08-11 17:53:05 +0530

    custom login

--- SpringSecEx/src/main/java/com/example/SpringSecEx/HelloController.java
+++ SpringSecEx/src/main/java/com/example/SpringSecEx/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.SpringSecEx;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/")
-    public String greet(){
-        return "Hello this is Himanshi!!";
+    public String greet(HttpServletRequest request){
+        return "Hello this is Himanshi!!" + request.getSession().getId();
     }
 }
 
