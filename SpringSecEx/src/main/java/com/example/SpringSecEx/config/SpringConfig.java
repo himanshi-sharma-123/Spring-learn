b34aba011b8539656c575586fc59d0af8e156514
Himanshi Sharma <himanshish456@gmail.com>
2024-08-11 21:15:37 +0530

    custom configuration

---
+++ SpringSecEx/src/main/java/com/example/SpringSecEx/config/SpringConfig.java
@@ -0,0 +1,40 @@
+package com.example.SpringSecEx.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity // go with this configuration
+public class SpringConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+//        http.csrf(customizer -> customizer.disable());
+
+        //******* BEHIND THE SCENES*****************//
+        Customizer<CsrfConfigurer<HttpSecurity>> custCsrf = new Customizer<CsrfConfigurer<HttpSecurity>>() {
+            @Override
+            public void customize(CsrfConfigurer<HttpSecurity> customizer) {
+                customizer.disable();
+            }
+        };
+        http.csrf(custCsrf);
+
+
+//        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+////        http.formLogin(Customizer.withDefaults());
+//        http.httpBasic(Customizer.withDefaults());
+//        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+
+        return http.build();
+    }
+
+}
