572db819172fd36ab3674253357fbe409a418a31
Himanshi Sharma <himanshish456@gmail.com>
2024-08-13 17:38:36 +0530

    request param, header and content negotiation

--- resful-web-services/src/main/java/com/example/resful_web_services/versioning/PersonV2.java
+++ resful-web-services/src/main/java/com/example/resful_web_services/versioning/PersonV2.java
@@ -2,35 +2,50 @@ package com.example.resful_web_services.versioning;
 
 public class PersonV2 {
 
-    private String firstName;
-    private String lastName;
+    private Name name;
 
-    public PersonV2(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+    public PersonV2(Name name) {
+        super();
+        this.name = name;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public Name getName() {
+        return name;
     }
 
     @Override
     public String toString() {
-        return "PersonV2{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                '}';
+        return "PersonV2 [name=" + name + "]";
     }
+//    private String firstName;
+//    private String lastName;
+//
+//    public PersonV2(String firstName, String lastName) {
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//    }
+//
+//    public String getFirstName() {
+//        return firstName;
+//    }
+//
+//    public void setFirstName(String firstName) {
+//        this.firstName = firstName;
+//    }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public void setLastName(String lastName) {
+//        this.lastName = lastName;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "PersonV2{" +
+//                "firstName='" + firstName + '\'' +
+//                ", lastName='" + lastName + '\'' +
+//                '}';
+//    }
 }
--- resful-web-services/src/main/java/com/example/resful_web_services/versioning/VersioningPersonController.java
+++ resful-web-services/src/main/java/com/example/resful_web_services/versioning/VersioningPersonController.java
@@ -3,16 +3,48 @@ package com.example.resful_web_services.versioning;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class VersioningPersonController {
-
     @GetMapping("/v1/person")
-    public PersonV1 getFirstVersionOfPerson(){
+    public PersonV1 getFirstVersionOfPerson() {
         return new PersonV1("Bob Charlie");
     }
 
     @GetMapping("/v2/person")
-    public PersonV2 getSecondVersionOfPerson(){
-        return new PersonV2("Bob", "Charlie");
+    public PersonV2 getSecondVersionOfPerson() {
+        return new PersonV2(new Name("Bob", "Charlie"));
     }
+
+    @GetMapping(path = "/person", params = "version=1") // http://localhost:8080/person?version=1
+    public PersonV1 getFirstVersionOfPersonRequestParameter() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path = "/person", params = "version=2") // http://localhost:8080/person?version=2
+    public PersonV2 getSecondVersionOfPersonRequestParameter() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=1")
+    public PersonV1 getFirstVersionOfPersonRequestHeader() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=2")
+    public PersonV2 getSecondVersionOfPersonRequestHeader() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 getFirstVersionOfPersonAcceptHeader() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 getSecondVersionOfPersonAcceptHeader() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+
 }
