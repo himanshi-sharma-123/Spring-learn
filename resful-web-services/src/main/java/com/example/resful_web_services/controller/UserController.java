c10b1aab56b00fa10ac1fb7ef6b2583d3d8d6843
Himanshi Sharma <himanshish456@gmail.com>
2024-08-13 13:45:36 +0530

    restful-web-services generic Exception handling for all resources

---
+++ resful-web-services/src/main/java/com/example/resful_web_services/controller/UserController.java
@@ -0,0 +1,45 @@
+package com.example.resful_web_services.controller;
+
+import com.example.resful_web_services.model.User;
+import com.example.resful_web_services.service.UserDaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserDaoService service;
+
+    public UserController(UserDaoService service){
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers(){
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable Integer id){
+        User user = service.findOne(id);
+        if(user == null)
+            throw new UserNotFoundException("id: " + id);
+        return user;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user){
+         User savedUser = service.save(user);
+         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                 .path("/{id}")
+                 .buildAndExpand(savedUser.getId())
+                 .toUri();
+         return ResponseEntity.created(location).build();
+    }
+}
---
+++ resful-web-services/src/main/java/com/example/resful_web_services/service/UserDaoService.java
@@ -0,0 +1,34 @@
+package com.example.resful_web_services.service;
+
+import com.example.resful_web_services.model.User;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserDaoService {
+    private static List<User> users = new ArrayList<>();
+
+    static{
+        users.add(new User(1, "Adam", LocalDate.now().minusYears(30)));
+        users.add(new User(2, "Eve", LocalDate.now().minusYears(25)));
+        users.add(new User(3, "Jim", LocalDate.now().minusYears(20)));
+    }
+
+    public List<User> findAll(){
+        return users;
+    }
+
+    public User findOne(Integer id) {
+        return users.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst().orElse(null);
+    }
+
+    public User save(User user){
+        users.add(user);
+        return user;
+    }
+}
