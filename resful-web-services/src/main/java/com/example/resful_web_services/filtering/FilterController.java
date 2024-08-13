be8452adba7e0013f084895f91c3fb7f4403f9b8
Himanshi Sharma <himanshish456@gmail.com>
2024-08-13 21:00:28 +0530

    static filtering for rest api

---
+++ resful-web-services/src/main/java/com/example/resful_web_services/filtering/FilterController.java
@@ -0,0 +1,21 @@
+package com.example.resful_web_services.filtering;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class FilterController {
+
+    @GetMapping("/filtering")
+    public SomeBean filtering(){
+        return new SomeBean("value1", "value2", "value3");
+    }
+
+    @GetMapping("/filtering-list")
+    public List<SomeBean> filteringList(){
+        return Arrays.asList(new SomeBean("value1", "value2", "value3"), (new SomeBean("value4", "value5", "value6"))) ;
+    }
+}
