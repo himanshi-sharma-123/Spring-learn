cb44a0fec0affc3f70acfc04232997284c6ac67d
Himanshi Sharma <himanshish456@gmail.com>
2024-08-15 18:47:05 +0530

    create quiz part 2

--- quizapp/src/main/java/com/example/quizapp/controller/QuizController.java
+++ quizapp/src/main/java/com/example/quizapp/controller/QuizController.java
@@ -1,5 +1,7 @@
 package com.example.quizapp.controller;
 
+import com.example.quizapp.service.QuizService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("quiz")
 public class QuizController {
 
+    @Autowired
+    private QuizService quizService;
+
     @PostMapping("/create")
     public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title){
-        return new ResponseEntity<>("quiz is here", HttpStatus.OK);
+        return quizService.createQuiz(category, numQ, title);
 
     }
 }
