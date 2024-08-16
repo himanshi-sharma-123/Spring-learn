4520cf4a8e06fa4ec3105eda9b25851484144e65
Himanshi Sharma <himanshish456@gmail.com>
2024-08-16 10:21:28 +0530

    running question service and make an instance of another port

--- question-service/src/main/java/com/example/question_service/controller/QuestionController.java
+++ question-service/src/main/java/com/example/question_service/controller/QuestionController.java
@@ -2,6 +2,8 @@ package com.example.question_service.controller;
 
 
 import com.example.question_service.model.Question;
+import com.example.question_service.model.QuestionWrapper;
+import com.example.question_service.model.Response;
 import com.example.question_service.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -29,5 +31,20 @@ public class QuestionController {
     @PostMapping("/add")
     public ResponseEntity<String> addQuestion(@RequestBody Question question){
         return questionService.addQuestion(question);
+    }
+
+    @GetMapping("/generate")
+    public ResponseEntity<List<Integer>> getQuestionsForQuiz(@RequestParam String categoryName, @RequestParam Integer numQuestions){
+        return questionService.getQuestionsForQuiz(categoryName, numQuestions);
+    }
+
+    @PostMapping("getQuestions")
+    public ResponseEntity<List<QuestionWrapper>> getQuestionsFromId(@RequestBody List<Integer> questionIds){
+        return questionService.getQuestionFromId(questionIds);
+    }
+
+    @PostMapping("/getScore")
+    public ResponseEntity<Integer> getScore(@RequestBody List<Response> responses){
+        return questionService.getScore(responses);
     }
 }
--- question-service/src/main/java/com/example/question_service/model/QuestionWrapper.java
+++ question-service/src/main/java/com/example/question_service/model/QuestionWrapper.java
@@ -1,8 +1,10 @@
 package com.example.question_service.model;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class QuestionWrapper {
     private Integer id;
 
