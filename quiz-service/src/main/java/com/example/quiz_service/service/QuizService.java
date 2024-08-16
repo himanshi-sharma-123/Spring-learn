a09322985a8ed85a6341ae16e244bb826cd98fb6
Himanshi Sharma <himanshish456@gmail.com>
2024-08-16 11:45:34 +0530

    working with feign

--- quiz-service/src/main/java/com/example/quiz_service/service/QuizService.java
+++ quiz-service/src/main/java/com/example/quiz_service/service/QuizService.java
@@ -1,6 +1,7 @@
 package com.example.quiz_service.service;
 
 import com.example.quiz_service.dao.QuizDao;
+import com.example.quiz_service.feign.QuizInterface;
 import com.example.quiz_service.model.Question;
 import com.example.quiz_service.model.QuestionWrapper;
 import com.example.quiz_service.model.Quiz;
@@ -20,44 +21,45 @@ public class QuizService {
     @Autowired
     private QuizDao quizDao;
 
-//    @Autowired
-//    private QuestionDao questionDao;
+    @Autowired
+    private QuizInterface quizInterface;
 
     public ResponseEntity<String> createQuiz(String category, int numQ, String title) {
-        List<Question> questions = // call the generate url in question service - RestTemplate will be use
-//
-//        Quiz quiz = new Quiz();
-//        quiz.setTitle(title);
-//        quiz.setQuestions(questions);
-//        quizDao.save(quiz);
+  //      List<Question> questions = // call the generate url in question service - RestTemplate will be use ==> short cut way is to use feign client
+
+        List<Integer> questions = quizInterface.getQuestionsForQuiz(category, numQ).getBody();
+        Quiz quiz = new Quiz();
+        quiz.setTitle(title);
+        quiz.setQuestionIds(questions);
+        quizDao.save(quiz);
 
         return new ResponseEntity<>("Success", HttpStatus.CREATED);
     }
 
     public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
         Optional<Quiz> quiz = quizDao.findById(id);
-        List<Question> questionsFromDb = quiz.get().getQuestions();
+//        List<Question> questionsFromDb = quiz.get().getQuestions();
         List<QuestionWrapper> questionsForUser = new ArrayList<>();
 
-        for(Question q: questionsFromDb){
-            QuestionWrapper qw = new QuestionWrapper(q.getId(), q.getQuestionTitle(), q.getOption1(), q.getOption2(), q.getOption3(), q.getOption4());
-            questionsForUser.add(qw);
-        }
+//        for(Question q: questionsFromDb){
+//            QuestionWrapper qw = new QuestionWrapper(q.getId(), q.getQuestionTitle(), q.getOption1(), q.getOption2(), q.getOption3(), q.getOption4());
+//            questionsForUser.add(qw);
+//        }
 
         return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
     }
 
     public ResponseEntity<Integer> calculateResult(Integer id, List<Response> responses) {
        Quiz quiz = quizDao.findById(id).get();
-       List<Question> questions = quiz.getQuestions();
+//       List<Question> questions = quiz.getQuestions();
        int right = 0, i = 0;
 
-       for(Response response: responses){
-           if(response.getResponse().equals(questions.get(i).getRightAnswer()))
-               right++;
-
-           i++;
-       }
+//       for(Response response: responses){
+//           if(response.getResponse().equals(questions.get(i).getRightAnswer()))
+//               right++;
+//
+//           i++;
+//       }
 
        return new ResponseEntity<>(right, HttpStatus.OK);
     }
